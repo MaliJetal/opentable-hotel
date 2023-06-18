70b7943fca20988c9fa3d8088d332666e89341ea
Jetal Mali <malijetal1234@gmail.com>
2023-06-18 23:46:09 +0530

    Setup of Db

--- opentable-app/app/page.tsx
+++ opentable-app/app/page.tsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
-import Link from "next/link";
-import { Navbar, Cards, Header } from "./components";
+import { Cards, Header } from "./components";
 
 export default function Home() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <main>
-          <Header />
-          {/* CARDS */}
-          <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-            {/* CARD */}
-            <Cards />
-            {/* CARD */}
-          </div>
-          {/* CARDS */}
-        </main>
-      </main>
+    <main>
+      <Header />
+      {/* CARDS */}
+      <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
+        {/* CARD */}
+        <Cards />
+        {/* CARD */}
+      </div>
+      {/* CARDS */}
     </main>
   );
 }
--- opentable-app/app/restaurant/[slug]/menu/page.tsx
+++ opentable-app/app/restaurant/[slug]/menu/page.tsx
@@ -1,21 +1,12 @@
-import Navbar from "../../../search/components/Navbar";
 import Header from "../components/Header";
 import RestaurantNavbar from "../components/RestaurantNavbar";
+import Menu from "../components/Menu";
 
 export default function RestaurantMenu() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <Header />
-        {/* DESCRIPTION PORTION */}
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[100%] rounded p-3 shadow'>
-            <RestaurantNavbar />
-          </div>
-        </div>
-        {/* DESCRIPTION PORTION */}
-      </main>
-    </main>
+    <div className='bg-white w-[100%] rounded p-3 shadow'>
+      <RestaurantNavbar />
+      <Menu />
+    </div>
   );
 }
